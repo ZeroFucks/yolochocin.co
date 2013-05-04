53bc4679f9e5c965e618e56ce8f956fee2cc977c
Lincoln Stoll <lstoll@lstoll.net>
2013-05-04 16:47:59 +0200

    Get the soundboard running.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    // Handler for .ready() called.
+    // Switch to soundboard
     $(".poopdiv").click(function () {
         $(this).hide();
         $(".soundboard").show();
+    });
+
+    // Player
+    $(".board-button").click(function () {
+        var mp3 = $(this).attr("data-mp3");
+        $("#mp3Source").attr("src", mp3).detach().appendTo("#player");
+        $("#player")[0].play();
     });
 });
